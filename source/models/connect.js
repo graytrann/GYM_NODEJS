83787f9a5dcb1ae4be4346cd02801c646a1642d2
graytrann <trankhaiphong987@gmail.com>
2024-04-08 09:06:06 +0700

    update sequelize

--- source/models/connect.js
+++ source/models/connect.js
@@ -1,13 +1,18 @@
 import { Sequelize } from "sequelize";
 
-const sequelize = new Sequelize("gym", "root", "1234", {
-  host: "localhost",
-  port: "3307",
-  dialect: "mysql",
-  pool: {
-    max: 10, // Ví dụ: Số lượng kết nối tối đa trong pool
-  },
-});
+const sequelize = new Sequelize(
+  "gym_elevenlaw",
+  "gym_elevenlaw",
+  "e6dc1031021385cd0247d72db24c8f125d6f6e3d",
+  {
+    host: "uee.h.filess.io",
+    port: "3307",
+    dialect: "mysql",
+    pool: {
+      max: 10, // Ví dụ: Số lượng kết nối tối đa trong pool
+    },
+  }
+);
 
 try {
   await sequelize.authenticate();
